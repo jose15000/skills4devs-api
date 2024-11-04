cb55a5488f44df2de8084749f4c6e6f77b3173ef
Jose Henrique <jose15000henrique@outlook.com>
2024-11-04 10:47:30 -0300

    a

--- src/DB/index.ts
+++ src/DB/index.ts
@@ -2,7 +2,11 @@ import mongoose from "mongoose";
 
 export async function connectToDatabase() {
   try {
-    await mongoose.connect("mongodb://localhost:27017/Test");
+    const mongoURL = Bun.env.MONGO_URL;
+    if (!mongoURL) {
+      return "Environment url was not defined.";
+    }
+    await mongoose.connect(mongoURL);
     console.log("Conectado ao MongoDB com sucesso!");
   } catch (error) {
     console.error("Erro ao conectar ao MongoDB:", error);
